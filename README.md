c12858f36b213777d71d00a8d592cc30c25eeaaf
haejin <haejin.yang91@gmail.com>
2023-04-17 18:07:36 +0900

    feat: window.location.pathname으로 라우팅

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # react-router-vanilla
 리액트에 바닐라 라우터를 달아본다
 
-## 1단계 기본세팅
+## 테스트용 UI 만들기
 따라해보다가 UI 만드는게 재밌어서 UI 만드는 것부터 시작
 
-## 라우팅 테스트
+## 1단계. 간단하게 라우팅 해보기
+window 객체의 location.pathname을 기반으로 라우팅을 한다.
+~~~
+const ShowAccordionList = () => {
+if (window.location.pathname === "/accordion") {
+return <AccordionList />;
+}
+}; 
+~~~
 
-## 라우팅 컴포넌트로 만들기
+## 2단계. 라우팅 컴포넌트로 만들기
 
-## 라우팅(새로고침없이)
+## 3단계. 라우팅(새로고침없이)
 
 ## 참고
 [링크](https://ncoughlin.com/posts/react-navigation-without-react-router/)
--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import { AccordionList } from "./component/Accordion/Accordion";
 import ColorSelector from "./component/ColorSelector/ColorSelector";
 import Interpreter from "./component/Interpreter/Interpreter";
 
+const ShowAccordionList = () => {
+  if (window.location.pathname === "/accordion") {
+    return <AccordionList />;
+  }
+};
+
+const ShowColorSelector = () => {
+  if (window.location.pathname === "/color-selector") {
+    return <ColorSelector />;
+  }
+};
+
+const ShowInterpreter = () => {
+  if (window.location.pathname === "/interpreter") {
+    return <Interpreter />;
+  }
+};
+
 function App() {
+  console.log(window.location.pathname);
   return (
-    <div>
-      <AccordionList />
-      <ColorSelector />
-      <Interpreter />
-    </div>
+    <>
+      {ShowAccordionList()}
+      {ShowColorSelector()}
+      {ShowInterpreter()}
+    </>
   );
 }
 
